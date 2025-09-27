f21f090598f336b175ffe6580e7b08e6dd7b782c
FernandoVini <araujofernando592@gmail.com>
2025-09-27 07:27:44 -0300

    verificacao do CPF e do telefone via regex

--- robo copy.py
+++ robo copy.py
@@ -1,28 +1,32 @@
 import pandas as pd
+import re
 
 df = pd.read_excel('dados_fake.xlsx')
 
 def validacao(df):
     erro = []
+    cpf = r'^\d{3}\.\d{3}\.\d{3}-\d{2}$'
+    celular = r"^(\+55\s?)?(\(?\d{2}\)?\s?)?\d{4,5}-?\d{4}$"
 
     for idx, linha in df.iterrows():
         if pd.isna(linha["Nome"]) or str(linha["Nome"]).strip() == "":
             erro.append(f"linha {idx} nome vazio")
 
 
-    for idx, linha in df.iterrows():
         if pd.isna(linha["Idade"]) or linha["Idade"] >= 120 or linha["Idade"] <= 0:
             erro.append(f"linha {idx} idade nao condinzente")
 
-
-    for idx, linha in df.iterrows():
-        if pd.isna(linha["CPF"]) or str(linha["CPF"]).strip() == "":
+        valor = str(linha["CPF"])
+        if not re.fullmatch(cpf , valor):
             erro.append(f" linha {idx} CPF nao preenchido corretamente")
             
-
-    for idx, linha in df.iterrows():
+            
         if linha["Situação"] not in ['Desempregado','Empregado']:
             erro.append(f"linha {idx} situacao preenchiada errada")
+
+        telefone = str(linha["Telefone"])
+        if not re.fullmatch(celular, telefone):
+            erro.append(f"linha {idx} telefone preenchido no formato errado")
 
 
     return erro
@@ -30,6 +34,7 @@ def validacao(df):
 df_final = validacao(df)
 for resp in df_final:
     print(resp)
+html = df.to_html("planilha.html" ,  index=False)
 
-print(df)
+print(html)
     
